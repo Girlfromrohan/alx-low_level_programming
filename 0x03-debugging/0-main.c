47a7b4db95fa920345da80ee0fd5c4bb56752fc3
Girlfromrohan <ragingraven100@gmail.com>
2022-10-19 14:14:48 -0700

    debugging commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * description: testing function that prints if int is negative or positive
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
