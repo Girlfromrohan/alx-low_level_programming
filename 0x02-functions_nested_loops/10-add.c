eb779f60bed99483f7661207cb261d747b8a2c3a
Girlfromrohan <ragingraven100@gmail.com>
2022-10-19 07:39:13 -0700

    commited today on nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - addition of two integers
+ * @a: int to be added
+ * @b: int to be added
+ * Return: sum of a and b
+ */
+
+
+
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_to_98 - prints numbers to 98
+ * @n: print from the int
+ * Return: always 0
+ */
+
+
+void print_to_98(int n)
+{
+	int a, b;
+
+	if (n <= 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			if (a != 98)
+				printf("%d, ", a);
+			else if (a == 98)
+				printf("%d\n", a);
+		}
+	}
+	else if (n >= 98)
+	{
+		for (b = n; b >= 98; b--)
+		{
+			if (b != 98)
+				printf("%d, ", b);
+			else if (b == 98)
+				printf("%d\n", b);
+		}
+	}
+}
+
