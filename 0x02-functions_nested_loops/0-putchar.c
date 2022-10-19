2cfb496b4420738217daf1424fd5e0344af24771
Girlfromrohan <ragingraven100@gmail.com>
2022-10-19 00:38:41 -0700

    commit message

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *
+ * main - Entry point
+ *
+ * Description: prints _putchar
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+
+{
+
+	char str[] = "_putchar\n";
+
+	int a = 0;
+
+
+
+	while (str[a] != '\0')
+
+		{
+
+			char c = str[a];
+
+			_putchar(c);
+
+			i++;
+
+			        	}
+
+	return (0);
+
+}
