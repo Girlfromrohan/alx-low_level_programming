2871d2dddee8e45a233706c2a51465a93c52b99d
Girlfromrohan <ragingraven100@gmail.com>
2022-10-19 04:47:41 -0700

    functions and loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- *
  * main - Entry point
  *
- * Description: prints _putchar
+ * Description: prints '_putchar\n'
  *
  * Return: Always 0 (success)
  *
@@ -16,21 +15,23 @@ int main(void)
 
 	char str[] = "_putchar\n";
 
-	int a = 0;
+	int i = 0;
 
 
 
-	while (str[a] != '\0')
+	while (str[i] != '\0')
 
-		{
+			{
 
-			char c = str[a];
+					char c = str[i];
 
-			_putchar(c);
 
-			i++;
 
-			        	}
+						_putchar(c);
+
+							i++;
+
+								}
 
 	return (0);
 
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+# include "main.h"
+
+/**
+ * print_alphabet - entry point
+ * description: print _alphabet
+ * Return: always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+
+
+
+
+
+	_putchar('\n');
+
+	
+
+}
